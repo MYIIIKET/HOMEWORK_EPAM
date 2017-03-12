08fd91ec0f96e8d7444654ae41bb7518df039ecc
MYIIIKET <mylllket@yandex.ua>
2017-03-12 21:51:18 +0300

    test for exception

--- PDF_HW_5/task5_2/src/test/java/com/mylllket_inc/app/PropetyReaderTest.java
+++ PDF_HW_5/task5_2/src/test/java/com/mylllket_inc/app/PropetyReaderTest.java
@@ -1,19 +1,20 @@
 package com.mylllket_inc.app;
 
 import com.mylllket_inc.app.Exceptions.NoKeyException;
-import com.mylllket_inc.app.Exceptions.NoPropertiesFileException;
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.io.IOException;
 
 
-public class PropetyReaderTest extends TestCase {
+public class PropetyReaderTest {
     public void testRead() throws Exception {
         PropetyReader a = new PropetyReader("src/main/java/com/mylllket_inc/app/SomeInfo.properties");
         String keys = a.read();
         System.out.println(keys);
     }
 
+    @Test(expected = NoKeyException.class)
     public void testNoKeyException() throws NoKeyException, IOException {
         PropetyReader a = new PropetyReader("src/main/java/com/mylllket_inc/app/CorruptedFile.properties");
         String keys = a.read();
